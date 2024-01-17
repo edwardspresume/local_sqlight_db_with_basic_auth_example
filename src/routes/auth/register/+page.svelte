e11b83ac38fef8f9d21363ed9b091329340e05c6
Edwards Presume <edwardspresume@gmail.com>
2024-01-17 17:19:56 -0500

    Add superForm and form fields for user registration

--- src/routes/auth/register/+page.svelte
+++ src/routes/auth/register/+page.svelte
@@ -1,5 +1,63 @@
 <script lang="ts">
 	import type { PageData } from './$types';
 
+	import { superForm } from 'sveltekit-superforms/client';
+
+	import { RegisterUserSchema } from '$validations/RegisterUserSchema';
+
+	import InputField from '$components/form/InputField.svelte';
+	import SubmitButton from '$components/form/SubmitButton.svelte';
+	import { route } from '$lib/ROUTES';
+	import Button from '$components/ui/button/button.svelte';
+
 	export let data: PageData;
+
+	const { enhance, errors, form } = superForm(data.registerUserFormData, {
+		taintedMessage: null,
+		validators: RegisterUserSchema
+	});
 </script>
+
+<div class="space-y-4 sm:grid sm:grid-cols-2 sm:justify-between sm:gap-10">
+	<section>
+		<h2 class="mb-4 text-2xl font-bold">Register</h2>
+
+		<form method="post" use:enhance class="space-y-4">
+			<InputField
+				type="text"
+				name="name"
+				label="Name"
+				bind:value={$form.name}
+				errorMessage={$errors.name}
+			/>
+
+			<InputField
+				type="email"
+				name="email"
+				label="Email"
+				bind:value={$form.email}
+				errorMessage={$errors.email}
+			/>
+
+			<InputField
+				type="password"
+				name="password"
+				label="Password"
+				bind:value={$form.password}
+				errorMessage={$errors.password}
+			/>
+
+			<SubmitButton />
+		</form>
+	</section>
+
+	<section>
+		<header class="flex justify-between">
+			<h2 class="mb-4 text-2xl font-bold">Registered users</h2>
+
+			<form method="post" action={route('deleteAllUsers /auth/register')}>
+				<Button type="submit">Delete all users</Button>
+			</form>
+		</header>
+	</section>
+</div>
