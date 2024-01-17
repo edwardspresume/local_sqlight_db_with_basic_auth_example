249c0075346e7307b6c9ba75add6ba45052d9cda
Edwards Presume <edwardspresume@gmail.com>
2024-01-17 15:26:55 -0500

    Remove feedbackForm from layout server load

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,16 +1,11 @@
 import type { LayoutServerLoad } from './$types';
 
-import { superValidate } from 'sveltekit-superforms/server';
-
 import { createBaseMetaTags } from '$lib/utils/metaTags';
-
-import { FeedbackValidationSchema } from '$validations/feedbackValidationSchema';
 
 export const load = (async ({ url }) => {
 	const baseMetaTags = createBaseMetaTags(url);
 
 	return {
-		baseMetaTags: Object.freeze(baseMetaTags),
-		feedbackForm: await superValidate(FeedbackValidationSchema)
+		baseMetaTags: Object.freeze(baseMetaTags)
 	};
 }) satisfies LayoutServerLoad;
