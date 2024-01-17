dc2f5dd2e10fce61f8b6abea74bff06df91b5bb7
Edwards Presume <edwardspresume@gmail.com>
2024-01-17 17:56:37 -0500

    Add actions and form validation to register page server

--- src/routes/auth/register/+page.server.ts
+++ src/routes/auth/register/+page.server.ts
@@ -1,5 +1,58 @@
-import type { PageServerLoad } from './$types';
+import type { Actions, PageServerLoad } from './$types';
+
+import { message, setError, superValidate } from 'sveltekit-superforms/server';
+
+import { checkIfEmailExists, insertNewUser } from '$lib/database/databaseUtils.server';
+import type { AlertMessageType } from '$lib/types';
+import { logError } from '$lib/utils';
+import { RegisterUserSchema } from '$validations/RegisterUserSchema';
 
 export const load = (async () => {
-	return {};
+	return {
+		registerUserFormData: await superValidate(RegisterUserSchema)
+	};
 }) satisfies PageServerLoad;
+
+export const actions: Actions = {
+	deleteAllUsers: async () => {},
+
+	registerUser: async ({ request }) => {
+		const registerUserFormData = await superValidate<typeof RegisterUserSchema, AlertMessageType>(
+			request,
+			RegisterUserSchema
+		);
+
+		if (registerUserFormData.valid === false) {
+			return message(registerUserFormData, {
+				alertType: 'error',
+				alertText: 'There was a problem with your submission.'
+			});
+		}
+
+		try {
+			const isEmailAlreadyRegistered = await checkIfEmailExists(registerUserFormData.data.email);
+
+			if (isEmailAlreadyRegistered === true) {
+				return setError(registerUserFormData, 'email', 'Email already exists.');
+			}
+
+			insertNewUser({
+				name: registerUserFormData.data.name,
+				email: registerUserFormData.data.email,
+				password: registerUserFormData.data.password
+			});
+
+			return message(registerUserFormData, {
+				alertType: 'success',
+				alertText: 'User registered successfully.'
+			});
+		} catch (error) {
+			logError(error);
+
+			return message(registerUserFormData, {
+				alertType: 'error',
+				alertText: 'There was an error with your submission'
+			});
+		}
+	}
+};
