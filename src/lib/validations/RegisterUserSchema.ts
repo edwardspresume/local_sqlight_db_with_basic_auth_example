194358e386f8dc5a15e50e8423af2b9d5e952005
Edwards Presume <edwardspresume@gmail.com>
2024-01-17 17:55:45 -0500

    Update RegisterUserSchema validation rules

--- src/lib/validations/RegisterUserSchema.ts
+++ src/lib/validations/RegisterUserSchema.ts
@@ -1,4 +1,8 @@
 import { usersTable } from '$lib/database/schema';
 import { createInsertSchema } from 'drizzle-zod';
 
-export const RegisterUserSchema = createInsertSchema(usersTable);
+export const RegisterUserSchema = createInsertSchema(usersTable, {
+	name: (schema) => schema.name.min(2),
+	email: (schema) => schema.email.email(),
+	password: (schema) => schema.password.min(2)
+});
