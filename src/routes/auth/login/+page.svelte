ede6b194ad59c8730e0581ed24f5eb54e8ace875
Edwards Presume <edwardspresume@gmail.com>
2024-01-18 12:15:02 -0500

    Add form validation and binding to login page

--- src/routes/auth/login/+page.svelte
+++ src/routes/auth/login/+page.svelte
@@ -1,15 +1,37 @@
 <script lang="ts">
 	import type { PageData } from './$types';
 
+	import { superForm } from 'sveltekit-superforms/client';
+
+	import { UserLogInSchema } from '$validations/UserLogInSchema';
+
 	import InputField from '$components/form/InputField.svelte';
 	import SubmitButton from '$components/form/SubmitButton.svelte';
 
 	export let data: PageData;
+
+	const { enhance, form, errors } = superForm(data.UserLoginFormData, {
+		resetForm: true,
+		taintedMessage: null,
+		validators: UserLogInSchema
+	});
 </script>
 
-<form class="space-y-4">
-	<InputField name="email" label="Email" type="email" />
-	<InputField name="password" label="Password" type="password" />
+<form class="space-y-4" method="post" use:enhance>
+	<InputField
+		name="email"
+		label="Email"
+		type="email"
+		bind:value={$form.email}
+		errorMessage={$errors.email}
+	/>
+	<InputField
+		name="password"
+		label="Password"
+		type="password"
+		bind:value={$form.password}
+		errorMessage={$errors.password}
+	/>
 
 	<SubmitButton />
 </form>
