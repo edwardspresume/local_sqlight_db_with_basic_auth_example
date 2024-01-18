135e7dfec505013d599c23777079cdcec962a8a8
Edwards Presume <edwardspresume@gmail.com>
2024-01-18 12:56:11 -0500

    feat(dashboard): add support for logging out in +page.svelte

--- src/routes/dashboard/+page.server.ts
+++ src/routes/dashboard/+page.server.ts
@@ -1,5 +1,27 @@
-import type { PageServerLoad } from './$types';
+import { redirect } from '@sveltejs/kit';
 
-export const load = (async () => {
-	return {};
+import type { Actions, PageServerLoad } from './$types';
+
+import { SESSION_COOKIE_NAME } from '$lib/constants';
+import { getUserName } from '$lib/database/databaseUtils.server';
+import { route } from '$lib/ROUTES';
+
+export const load = (async ({ cookies }) => {
+	const userId = cookies.get(SESSION_COOKIE_NAME);
+
+	if (!userId) throw redirect(303, route('/auth/login'));
+
+	return {
+		loggedOnUser: await getUserName(userId)
+	};
 }) satisfies PageServerLoad;
+
+export const actions: Actions = {
+	default: async ({ cookies }) => {
+		cookies.delete(SESSION_COOKIE_NAME, {
+			path: route('/')
+		});
+
+		throw redirect(303, route('/auth/login'));
+	}
+};
--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -1,5 +1,15 @@
 <script lang="ts">
 	import type { PageData } from './$types';
 
+	import SubmitButton from '$components/form/SubmitButton.svelte';
+
 	export let data: PageData;
 </script>
+
+<h1>
+	Hello {data.loggedOnUser}
+</h1>
+
+<form method="post">
+	<SubmitButton>Logout</SubmitButton>
+</form>
