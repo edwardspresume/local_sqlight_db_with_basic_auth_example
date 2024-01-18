88c6f7bfa3cfa9e6fce694b08ba74a6e173ca3b7
Edwards Presume <edwardspresume@gmail.com>
2024-01-18 12:08:40 -0500

    Add form with input fields and submit button for login page

--- src/routes/auth/login/+page.svelte
+++ src/routes/auth/login/+page.svelte
@@ -1,5 +1,15 @@
 <script lang="ts">
 	import type { PageData } from './$types';
 
+	import InputField from '$components/form/InputField.svelte';
+	import SubmitButton from '$components/form/SubmitButton.svelte';
+
 	export let data: PageData;
 </script>
+
+<form class="space-y-4">
+	<InputField name="email" label="Email" type="email" />
+	<InputField name="password" label="Password" type="password" />
+
+	<SubmitButton />
+</form>
