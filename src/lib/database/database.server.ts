58de2bed2c85b2d5bbf67f91168681208dee238e
Edwards Presume <edwardspresume@gmail.com>
2024-01-19 18:38:04 -0500

    Remove unnecessary import and schema from database.server.ts

--- src/lib/database/database.server.ts
+++ src/lib/database/database.server.ts
@@ -1,10 +1,6 @@
 import Database from 'better-sqlite3';
 import { drizzle } from 'drizzle-orm/better-sqlite3';
 
-import * as schema from './schema';
-
 const sqliteClient = new Database('databaseStorage/sqlite.db');
 
-export const database = drizzle(sqliteClient, {
-	schema
-});
+export const database = drizzle(sqliteClient);
